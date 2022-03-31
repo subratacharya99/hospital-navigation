ea56bfb7b7b591120a4d6b93ae5e21f03abc121a
subratacharya99 <subratacharya99@gmail.com>
2022-03-31 15:43:18 -0500

    created createElevator functionality

--- methodtesting.py
+++ methodtesting.py
@@ -5,25 +5,45 @@ from neo4j import GraphDatabase
 # Node of type elevator will have 
 
 class Test:
+    
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        print("Connected To Database")
 
     def close(self):
         self.driver.close()
+        print("Disconnected")
     
+    #This function works, but does not check for duplicates
     def createElevator(self, elevatorName, floor):
         with self.driver.session() as session:
             query = (
-                f"MATCH (e: Elevator {{name: {elevatorName}, floor: {floor} }})"
-                "RETURN e.name AS name, e.floor AS floor"
+                f"MERGE (e: Elevator {{ name: '{elevatorName}' }})"
+                f"SET e.floor = 1"
+                #"RETURN e"
             )
-            result = session.run(query)
-            print(f"{result['name']} on floor {result['floor']} has been added!")
+            session.run(query)
+            print(f"Elevator {elevatorName} on floor {floor} has been added!")
+
+    def setElevatorElevatorRelationship(self, firstElevator, secondElevator, floor, relationship):
+        with self.driver.session() as session:
+            query = (
+                f"MATCH (e1: Elevator {{ name: '{firstElevator}', floor: {floor}}})"
+                f"MATCH (e2: Elevator {{name: '{secondElevator}', floor: {floor}}})"
+                f"MERGE (e1)-[r: '{relationship}']-(e2)"
+                "RETURN e1, e2, r"
+            )
+            result= session.run(query)
+            print(result)
+
+
+#relationships
+vertical = "north_south"
+horizontal = "east_west"
 
 
 
-
-if __name__ == "main":
-    test = Test("neo4j+s://2e126d37.databases.neo4j.io", "neo4j", "fMMMCrLRM3buP_V1EfNj3AVMhuqKRHmdJHvjPp2C51A")
-    test.createElevator("I", 1)
-    test.close()
+testing = Test("neo4j+s://2e126d37.databases.neo4j.io", "neo4j", "fMMMCrLRM3buP_V1EfNj3AVMhuqKRHmdJHvjPp2C51A")
+#testing.createElevator("A", 1)
+testing.setElevatorElevatorRelationship("A", "B", 1, vertical)
+testing.close()
