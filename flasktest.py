d4a901ef8a7af968f1a2d8c2d7fe2071665b6c57
subratacharya99 <subratacharya99@gmail.com>
2022-04-05 15:55:56 -0500

    created add_location function

--- flasktest.py
+++ flasktest.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import LocationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8c77bf14abe1c038b49ffb7087067fbe'
 
 #If we make git public, then these should be changed to environment variables
 user = 'neo4j'
@@ -12,6 +14,11 @@ uri = '2e126d37.databases.neo4j.io'
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/add_location")
+def add_location():
+    form = LocationForm()
+    return render_template('add_location.html', title = 'Add a Location', form = form)
 
 
 if __name__ == '__main__':
