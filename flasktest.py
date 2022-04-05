eea42203141d93bcc0c41e92172df600f06b7d80
subratacharya99 <subratacharya99@gmail.com>
2022-04-05 15:43:03 -0500

    removed neomodel and modified form file

--- flasktest.py
+++ flasktest.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, url_for
-import neomodel
-from neomodel import config
 
 app = Flask(__name__)
 
@@ -9,19 +7,6 @@ user = 'neo4j'
 pw = 'fMMMCrLRM3buP_V1EfNj3AVMhuqKRHmdJHvjPp2C51A'
 uri = '2e126d37.databases.neo4j.io'
 
-db_url = 'neo4j+s://{}:{}@{}'.format(user, pw, uri)
-
-config.DATABASE_URL = db_url
-
-
-class Location(neomodel.StructuredNode):
-    __primarykey__ = 'name'
-    name = neomodel.StringProperty(unique_index=True)
-
-
-def create_an_item():
-    neomodel.db.set_connection(db_url)
-    Location(name='Elevator E').save()
 
 
 @app.route("/")
--- forms.py
+++ forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired, Length
 
 class LocationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2)])
-    accessible = BooleanField('Accessible', validators=[DataRequired()])
     floor = IntegerField('Floor', validators=[DataRequired()])
-    
+    building = StringField('Building', validators = Length(min=2, max=20))
+    submit = SubmitField('Confirm')
